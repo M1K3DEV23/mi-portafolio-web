db7cc868c147f6610cc1242e05ab8dabd2669383
M1K3DEV23 <mikery2310@outlook.com>
2024-12-02 14:35:16 -0600

    Configurando para GITHUBPAGES

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://M1K3DEV23.github.io',
+    base: 'mi-portafolio-web'
+});
